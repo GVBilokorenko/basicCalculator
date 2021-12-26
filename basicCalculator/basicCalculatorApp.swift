37171d62e2c6623e7c4d9280722dbe81d43011ce
Artem <abilokorenko@gmail.com>
2021-12-26 18:15:06 +0300

    initial commit

---
+++ basicCalculator/basicCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  basicCalculatorApp.swift
+//  basicCalculator
+//
+//  Created by Admin on 26.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct basicCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
